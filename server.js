a815fb53d8a81e8bfb25d8abe9ab3bf72e635f01
Cody Fackrell <codyfackrell56@gmail.com>
2023-03-30 20:09:19 -0600

    updated public folder

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const SERVER_PORT = 4884
 
-app.use(express.static(`${__dirname}/client`))
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
