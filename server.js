6069535d56008f0093c3f5213edbbea7e691737f
Cody Fackrell <codyfackrell56@gmail.com>
2023-03-30 19:42:21 -0600

    updated server port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const SERVER_PORT = 4884
 
 app.use(express.static(`${__dirname}/client`))
 
-app.listen(4848, () => console.log('Server running on 4848'))
+app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
